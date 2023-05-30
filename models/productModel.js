5a40733e2f72cb060004c2e2a975f2fa470dc09a
Aniket1103 <55594740+Aniket1103@users.noreply.github.com>
2023-05-31 00:05:53 +0530

    connected Database and developed CRUD operations APIs

---
+++ models/productModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema(
+    {
+        "name" : {
+            "type" : String,
+            "required" : [true, "Please Enter a name."]
+        },
+        "quantity" : {
+            "type" : Number,
+            "required" : true
+        },
+        "price" : {
+            "type" : Number,
+            "required" : true
+        },
+        "image" : {
+            "type" : String,
+            "required" : false
+        }
+    },
+    {
+        timeStamps : true
+    }
+)
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
--- server.js
+++ server.js
@@ -1,13 +1,75 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Product = require("./models/productModel");
 const app = express();
+
+app.use(express.json());    //this middleware enables accessing body data passed in JSON format
+app.use(express.urlencoded({extended : false}));    //this middleware allows accessing body data passed through form data
 
 app.get("/", (req, res) => {
     res.send("Hello Node API");
 })
 
-app.get("/blog", (req, res) => {
-    res.send("Hello this is blog page.");
+app.get("/products", async (req, res) => {
+    try{
+        const products = await Product.find({});
+        res.status(200).json({products : products});
+    }
+    catch(error){
+        res.status(500).json({message : error.message});
+    }
+})
+
+app.get("/products/:id", async (req, res) => {
+    try{
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    }
+    catch(error){
+        res.status(500).json({message : error.message});
+    }
+})
+
+app.post("/products", async (req, res) => {
+    console.log(req.body);
+    try{
+        const product = await Product.create(req.body);
+        res.status(201).json(product);
+    }
+    catch(error){
+        res.status(500).json({message : error.message});
+    }
+})
+
+app.put("/products/:id", async (req, res) => {
+    try{
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        if(!product) {
+            res.status(404).json({message : `Cannot find any product with id ${id}`});
+        }
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+    }
+    catch(error){
+        res.status(500).json({message : error.message});
+    }
+})
+
+
+app.delete("/products/:id", async (req, res) => {
+    try{
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product) {
+            res.status(404).json({message : `Cannot find any product with id ${id}`});
+        }
+        res.status(200).json(product);
+    }
+    catch(error){
+        res.status(500).json({message : error.message});
+    }
 })
 
 mongoose.set("strictQuery", false)
